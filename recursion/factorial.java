a14c517fe7b4cc9b38b88145e54202c1083141c9
Uday Sharma <udaysharma.2350@gmail.com>
2026-03-05 20:58:49 +0530

    Renamed tree problems and cleaned repo structure

--- recursion/factorial.java
+++ recursion/factorial.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class factorial{
     static int fact(int n){
         if(n==0) return 1;
@@ -7,6 +9,10 @@ public class factorial{
     }
 
     public static void main(String[] args){
-        System.out.println(fact(5));
+        // System.out.println(fact(5));
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(fact(a));
+        sc.close();
     }
 }
