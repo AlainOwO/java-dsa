e8689e356f90ec6b1cb604e59f3f63cd58d24513
Uday Sharma <udaysharma@Udays-MacBook-Air.local>
2026-03-05 13:23:29 +0530

    Solved MaximumDepth of a Binary Tree

---
+++ trees/maxdepth.java
@@ -0,0 +1,39 @@
+package trees;
+// import java.util.Scanner;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class maxdepth{
+
+    // You only modify this function
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left, right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        // Scanner sc = new Scanner(System.in);
+        int depth = maxDepth(root);
+
+        System.out.println("Maximum Depth: " + depth);
+    }
+}
