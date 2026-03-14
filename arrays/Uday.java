ce247b062a984b9352f1252ae8cff1eef9a75eab
Uday Sharma <udaysharma.2350@gmail.com>
2026-03-14 18:04:35 +0530

    fibonacci number

--- arrays/Uday.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class Uday {
-	public static void main(String [] args) {
-
-	System.out.println("UDay is the chepest Whore 450");
-	}
-}
---
+++ recursion/fibonacci.java
@@ -0,0 +1,19 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class fibonacci {
+    public static int fibo(int n){
+        if(n==1||n==0){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args){
+        // System.out.println(fact(5));
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(fibo(a));
+        sc.close();
+    }
+}
